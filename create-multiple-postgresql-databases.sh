7ea86aa0b5ac105ae174084e4dda13fa8de23e0a
Shadaï ALI <nafiouali@gmail.com>
2019-10-25 20:02:12 +0100

    add support for POSTGRES_USER and POSTGRES_PASSWORD envs

--- create-multiple-postgresql-databases.sh
+++ create-multiple-postgresql-databases.sh
@@ -5,18 +5,21 @@ set -u
 
 function create_user_and_database() {
 	local database=$1
+	local user=$2 
+	local password=$3
+	
 	echo "  Creating user and database '$database'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $database;
+	    CREATE $user USER WITH ENCRYPTED PASSWORD $password;
 	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $user TO $database;
 EOSQL
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-		create_user_and_database $db
+		create_user_and_database $db $POSTGRES_USER $POSTGRES_PASSWORD
 	done
 	echo "Multiple databases created"
 fi
